3058da46618f7da5f0281b58d5194d12adce6c50
wang <zhao.wang.gc@hco.ntt.co.jp>
2025-10-12 14:28:38 +0900

    Reduce mobile homepage bottom spacer to 96px

--- components/hero.tsx
+++ components/hero.tsx
@@ -138,7 +138,7 @@ export function Hero() {
       </div>
       
       {/* Spacer for mobile to push content away from footer */}
-      <div className="h-48 sm:h-0"></div>
+      <div className="h-24 sm:h-0"></div>
     </div>
   );
 }
